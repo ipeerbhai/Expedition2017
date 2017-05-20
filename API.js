fc03dd01ea80c714e94b6b85b329734841286c79
ipeerbhai <ipeerbhai@aol.com>
2017-05-20 14:48:16 -0700

    increasing scaffold to have the first API call.

--- API.js
+++ API.js
@@ -8,9 +8,66 @@ const fs = require('fs');
 const path = require('path');
 var jsonlines = require('jsonlines');
 var parser = jsonlines.parse();
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+// Private functions
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------
+// Authorize checks that the API call is properly allowed to use this API.
+function Authorize(req, res) {
+    var AuthorizationHeader = req.header("Authorization");
+    var _clientID = req.header("X-Client-ID");
+    var AllowedToUseAP = false;
+
+    if (AuthorizationHeader === "ExpiditionHack2017") {
+        AllowedToUseAP = true;
+    }
+    else {
+        // we are not authorized.  Send a forbidden code (403)
+        res.status = 403;
+        res.send(); // this will end the session, and 
+    }
+
+    // Check authorized client.  If not, close out the tunnel.
+    var Authorization = {
+        IsAuthroized: AllowedToUseAP,
+        ClientID: _clientID
+    };
+
+    return (Authorization);
+}
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+// Exported functions
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------
+// Exported Function:
+//  Input a string to this function, get back an automatic summarization.
+var SummarizeText = function (req, res) {
+    var AuthToken = Authorize(req, res);
+    if (!AuthToken.IsAuthroized) {
+        return;
+    }
+
+    var _Message = req.body.Message;
+
+    // Run Gensim and get a result
+    // var GensimSummary = RunGenSim(_Message)
+
+    // Return the string to the caller.
+    res.setHeader('content-type', contentType);
+    // res.write(GensimSumary);
+    res.status = 200;
+    res.send();
+}
 
 //---------------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------------------------------
 // Exports
 //---------------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------------------------------
+module.exports.SummarizeText = SummarizeText;
