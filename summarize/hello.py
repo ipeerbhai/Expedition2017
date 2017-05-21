8290d1ac8e3313e304d0ea6387bb51a8aa3dc46e
Daniel Blackburn <craklyn@gmail.com>
2017-05-21 11:09:55 -0700

    Now can injest list of lists

--- summarize/hello.py
+++ summarize/hello.py
@@ -1,7 +1,15 @@
 import csv
 from gensim.summarization import summarize
+import json
+import sys
 
 print("hello")
+
+# 
+#args = '[[162,191,838,1431,1433,1446,1493,1522,17,27],[1496,130],[50,91,95,1444,1464,13,15],[21,129,160,310,314,316,332,5],[189,190,131]]'
+args = sys.argv[1:]
+articleList = json.loads(str(args[0]))
+print(articleList)
 
 ifile = open('C:\\Users\\soth02\\Documents\\Expedition\\expedition\\Expedition2017\\data\\NYTimesArticles.csv',"r",encoding='utf8')
 reader = csv.reader(ifile)
@@ -46,3 +54,5 @@ print("<br/>")
 print(summarize(text[1] + text[0], word_count=200))
 
 #print(headers[3])
+
+
