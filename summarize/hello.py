14e8e72d5dfe364e5ca097ddc143a7c09613b3dc
Daniel Blackburn <craklyn@gmail.com>
2017-05-24 20:37:49 -0700

    Fixed off-by-one error"

--- summarize/hello.py
+++ summarize/hello.py
@@ -6,17 +6,14 @@ import json
 import sys
 import pandas
 
-
-# 
-#args = '[[162,191,838,1431,1433,1446,1493,1522,17,27],[1496,130],[50,91,95,1444,1464,13,15],[21,129,160,310,314,316,332,5],[189,190,131]]'
 args = sys.argv[1:]
+# Example args
+#args = '[[162,191,838,1431,1433,1446,1493,1522,17,27],[1496,130],[50,91,95,1444,1464,13,15],[21,129,160,310,314,316,332,5],[189,190,131]]'
+
 articleList = json.loads(str(args[0]))
 #print(articleList)
 
-#ifile = open('C:\\Users\\soth02\\Documents\\Expedition\\expedition\\Expedition2017\\data\\NYTimesArticles_woAds.csv',"r",encoding='utf8')
-
 df = pandas.read_csv('data/NYTimesArticles_woAds.csv')
-
 
 masterArticles = []
 masterTitles = []
@@ -29,15 +26,15 @@ for articleGroup in articleList:
   image = ''
   urlCount = 1
   for article in articleGroup:
-    concatedArticles = concatedArticles + df['text'][article-1]
-    tempURL = df['url'][article-1].strip()
-    concatedURLs = concatedURLs + '<li><a href="' + tempURL + '">' + tempURL + '</a>' + '</li>'
-#    concatedURLs = concatedURLs + '<a href="' + tempURL + '">' + str(urlCount) + ": " + tempURL + '</a>' + '<br />'
+    concatedArticles = concatedArticles + df['text'][article] + '\n'
+    tempURL = df['url'][article].strip()
+    concatedURLs += '<li><a href="' + tempURL + '">' + tempURL + '</a>'
+    concatedURLs += '<br /><p style=\"font-size:9px\">Keywords: ' + df['keywords'][article] + '</p></li>'
     urlCount = urlCount + 1
-    if (df['top_image'][article-1] != ''):
-      image = df['top_image'][article-1]
+    if (df['top_image'][article] != ''):
+      image = df['top_image'][article]
   masterArticles.append(concatedArticles)
-  masterTitles.append(df['summary'][articleGroup[0]-1])
+  masterTitles.append(df['summary'][articleGroup[0]])
   masterImages.append(image)
   masterArticleURLs.append(concatedURLs)
 
